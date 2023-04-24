0a0044e712bb1d36f2803b86e001b30286381321
kaldray <jordray@protonmail.com>
2023-04-24 15:39:22 +0200

    feat(factories): create factories for all tables

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->name(),
+            "description" => fake()->sentence(10),
+            "price" => fake()->randomFloat(2),
+            "image" => fake()->imageUrl(200, 200),
+            "published" => fake()->randomElement(['publié', "non publié"]),
+            "state" => fake()->randomElement(['solde', "standard"]),
+            "reference" => fake()->word(),
+        ];
+    }
+}
