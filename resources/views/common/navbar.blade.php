9a7731c25fcb6ecafa6123f4b702731c4a65cf0b
kaldray <jordray@protonmail.com>
2023-04-26 11:15:13 +0200

    style: add bg color to navBar

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed top-0 mb-5 w-full">
+<nav class="fixed top-0 mb-5 w-full bg-black">
   <section
     class="mx-auto flex max-w-2xl flex-row flex-wrap justify-between gap-5 p-5"
   >
@@ -6,12 +6,12 @@
       <a class="text-logo" href="{{ route('index')}} ">WeFashion</a>
     </div>
     <div class="hamburger space-y-2 sm:hidden">
-      <div class="h-0.5 w-8 bg-gray-600"></div>
-      <div class="h-0.5 w-8 bg-gray-600"></div>
-      <div class="h-0.5 w-8 bg-gray-600"></div>
+      <div class="h-0.5 w-8 bg-white"></div>
+      <div class="h-0.5 w-8 bg-white"></div>
+      <div class="h-0.5 w-8 bg-white"></div>
     </div>
     <div
-      class="nav-menu group-[]: hidden basis-full flex-col items-center justify-center gap-5 sm:block sm:basis-auto"
+      class="nav-menu group-[]: hidden basis-full flex-col items-center justify-center gap-5 text-white sm:block sm:basis-auto"
     >
       <a href="">Soldes</a>
       <a href="">Homme</a>
