c6e4164762990a7015d4074dc286d75e7fe2c8f5
kaldray <jordray@protonmail.com>
2023-04-26 10:32:35 +0200

    feat: create home  page with product

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends("base") @section("main")
+<div
+  class="mx-auto mt-5 grid w-4/5 grid-cols-12 place-content-center justify-items-center gap-5"
+>
+  @foreach ($products as $p )
+  <div class="col-span-12 sm:col-span-4">
+    <img class="w-full" src="{{ $p->image }}" alt="Description image" />
+    <p class="text-center">{{ $p->name }}</p>
+    <p class="text-center">{{ $p->price }} €</p>
+  </div>
+  @endforeach
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('base');
-});
+Route::resource("/", ProductController::class);
