887277e0665532b87cebe6ec6c657fac702aeb58
kaldray <jordray@protonmail.com>
2023-04-28 14:03:32 +0200

    fix: make refence as unique

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -11,25 +11,26 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+  protected $model = Product::class;
 
-    protected $model = Product::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            "name" => fake()->name(),
-            "description" => fake()->sentence(10),
-            "price" => fake()->randomFloat(2, 0, 100),
-            "image" => fake()->imageUrl(200, 200),
-            "published" => fake()->randomElement(['publié', "non publié"]),
-            "state" => fake()->randomElement(['solde', "standard"]),
-            "reference" => fake()->unique()->text(16),
-            "categories_id" => Categories::all()->random()->id
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      "name" => fake()->name(),
+      "description" => fake()->sentence(10),
+      "price" => fake()->randomFloat(2, 0, 100),
+      "image" => fake()->imageUrl(200, 200),
+      "published" => fake()->randomElement(["publié", "non publié"]),
+      "state" => fake()->randomElement(["solde", "standard"]),
+      "reference" => fake()
+        ->unique()
+        ->regexify("[A-Za-z0-9]{16}"),
+      "categories_id" => Categories::all()->random()->id,
+    ];
+  }
 }
