6eac23827afeb1c35e2c3bef72e9d1751e6ed9ec
kaldray <jordray@protonmail.com>
2023-04-29 19:42:46 +0200

    feat: update Product factory to seed img from storage to db

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Categories;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
@@ -20,11 +21,13 @@ class ProductFactory extends Factory
    */
   public function definition(): array
   {
+    $files = Storage::disk("public")->allFiles("products");
+    $randomFiles = $files[rand(0, count($files) - 1)];
     return [
       "name" => fake()->name(),
       "description" => fake()->sentence(10),
       "price" => fake()->randomFloat(2, 0, 100),
-      "image" => fake()->imageUrl(200, 200),
+      "image" => $randomFiles,
       "published" => fake()->randomElement(["publié", "non publié"]),
       "state" => fake()->randomElement(["solde", "standard"]),
       "reference" => fake()
